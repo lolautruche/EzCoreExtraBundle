7abeabc5625483453c3aa60211e62a7694819639
Jérôme Vieilledent <lolautruche@gmail.com>
2016-05-18 10:06:10 +0200

    Added default_settings.yml

--- DependencyInjection/EzCoreExtraExtension.php
+++ DependencyInjection/EzCoreExtraExtension.php
@@ -22,5 +22,6 @@ class EzCoreExtraExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        $loader->load('default_settings.yml');
     }
 }
